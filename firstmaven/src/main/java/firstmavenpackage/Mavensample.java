f2fa0010e146d1238def682834833af1a6929b6a
admin <tvmaravind@gmail.com>
2024-08-07 20:55:46 +0530

    Sample Push

---
+++ firstmaven/src/main/java/firstmavenpackage/Mavensample.java
@@ -0,0 +1,11 @@
+package firstmavenpackage;
+
+public class Mavensample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HELLO");
+	}
+
+}
